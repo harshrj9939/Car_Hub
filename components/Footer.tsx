898f518715cd4e149c495d4fbd089923bbf236a7
Harsh raj <harshrj9939@gmail.com>
2024-07-08 00:07:34 +0530

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from "next/link"; 
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      
+    </footer>
   )
 }
 
