be741e87c2cb26f06f4af7c6306cd3fb6e408d1a
Harsh raj <harshrj9939@gmail.com>
2024-07-08 00:12:29 +0530

    final

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,13 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer>
-      
+    <footer className = "flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      <div className = " flex max-md: flex-col flex-wrap justify-between gep-5 sm:px-16 px-6 py-10">
+        <div className= "felx felx-col justify-start items-start gap-6">
+          
+        </div>
+      </div>
+
     </footer>
   )
 }
